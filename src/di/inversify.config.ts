17a2a57226ed8bc1c948983027754251b560ce50
Kuye Harry <dammykuye@gmail.com>
2023-07-04 23:08:56 +0100

    add container binds for auth module

--- src/di/inversify.config.ts
+++ src/di/inversify.config.ts
@@ -1,11 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 import {Container} from "inversify";
-import { AuthTypes, IAuthRepository } from "../modules/auth/auth.dto";
+import { AuthTypes, IAuthRepository, IAuthService } from "../modules/auth/auth.dto";
 import AuthRepository from "../modules/auth/auth.repository";
+import { AuthService } from "../modules/auth/auth.service";
 
 const container = new Container();
 
 container.bind(PrismaClient).toConstantValue(new PrismaClient());
 container.bind<IAuthRepository>(AuthTypes.IAuthRepository).to(AuthRepository);
+container.bind<IAuthService>(AuthTypes.IAuthService).to(AuthService);
+
 
 export default container;
