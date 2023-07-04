73faa7eabee69d6f7c5c382514c8c38d1a5f2113
Kuye Harry <dammykuye@gmail.com>
2023-07-04 23:15:08 +0100

    add container binds for module module

--- src/di/inversify.config.ts
+++ src/di/inversify.config.ts
@@ -3,12 +3,16 @@ import {Container} from "inversify";
 import { AuthTypes, IAuthRepository, IAuthService } from "../modules/auth/auth.dto";
 import AuthRepository from "../modules/auth/auth.repository";
 import { AuthService } from "../modules/auth/auth.service";
+import { IEmailQueue, IMailService, MailTypes } from "../modules/mail/mail.interface";
+import EmailQueue from "../modules/mail/queuehandler";
+import MailService from "../modules/mail/mail.service";
 
 const container = new Container();
 
 container.bind(PrismaClient).toConstantValue(new PrismaClient());
 container.bind<IAuthRepository>(AuthTypes.IAuthRepository).to(AuthRepository);
 container.bind<IAuthService>(AuthTypes.IAuthService).to(AuthService);
-
+container.bind<IEmailQueue>(MailTypes.IEmailQueue).to(EmailQueue);
+container.bind<IMailService>(MailTypes.IMailService).to(MailService);
 
 export default container;
