bb630fa1a00c9502f2217a1a6b2b58b8b7ad45e6
Kuye Harry <dammykuye@gmail.com>
2023-07-14 14:57:28 +0100

    remove exlude key utls

--- src/utils/db/excludeKey.ts
+++
@@ -1,7 +0,0 @@
-import { User } from "@prisma/client";
-import { UserProfile } from "../../modules/auth/auth.interface";
-
-export default function removePassword(user: User): UserProfile {
-  const { password, ...userWithoutPassword } = user;
-  return userWithoutPassword;
-}
