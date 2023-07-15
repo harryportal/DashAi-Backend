90a36d814774bce4a352028abbf8c3dd9cba3a9c
Kuye Harry <dammykuye@gmail.com>
2023-07-15 13:00:27 +0100

    include external fields in gift model

---
+++ src/utils/db/seed/tag/seedtag-json.ts
@@ -0,0 +1,43 @@
+export const tags = [
+    {
+      "id": "3e2c3e25-4f22-4fd2-87f6-95191f9c2090",
+      "name": "Birthday"
+    },
+    {
+      "id": "840e7427-4577-4e6d-b833-52bda582ca2a",
+      "name": "Anniversary",
+    },
+    {
+      "id": "1751472b-39de-4df9-8443-72e77a14f928",
+      "name": "Congratulations",
+    },
+    {
+      "id": "f1a15e7d-747a-4f96-b413-72b71dcbf3b1",
+      "name": "Love",
+    },
+    {
+      "id": "f27e20d5-8d34-49b2-835b-1b69f0b29a3d",
+      "name": "Friendship",
+    },
+    {
+      "id": "9f99dbf4-8f53-48a7-b7df-1a31562ab84a",
+      "name": "Thank You",
+    },
+    {
+      "id": "e5f99aeb-9032-4e86-b64f-6072dc1eb034",
+      "name": "Get Well Soon",
+    },
+    {
+      "id": "7e29e2a7-bd8f-4a7b-98e7-6038a68b2285",
+      "name": "Graduation",
+    },
+    {
+      "id": "96a5f982-fc4a-4c4e-b16e-3699eb8a7102",
+      "name": "New Baby",
+    },
+    {
+      "id": "8ab3c477-0110-4e53-b58e-33029e9b9f16",
+      "name": "Wedding",
+    }
+  ]
+  
---
+++ src/utils/db/seed/tag/seedtag.ts
@@ -0,0 +1,23 @@
+import logger from "../../../logging/winston"
+import { prisma } from "../../prisma"
+import { tags } from "./seedtag-json"
+
+const createTag = async()=>{
+    for (const tag of tags){
+        await prisma.tag.create({
+            data: tag
+        })
+    }
+}
+
+export const seedTag = async()=>{
+    try{
+        await createTag();
+        logger.info("Successfully Added Dummy Tags to the Database");
+    }catch(err:any){
+        logger.error("Error adding tags to the Database", err);
+    }
+}
+
+seedTag();
+
