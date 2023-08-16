788f79b46101fa0e9c1cacada5d3e5aec7e15041
Kuye Harry <dammykuye@gmail.com>
2023-08-16 23:36:06 +0100

    fix onboarding

--- src/modules/user/user.router.ts
+++ src/modules/user/user.router.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { protect } from "../../common/auth";
 import container from "../../di/inversify.config";
 import UserController from "./user.controller";
+import RequestValidator from "../../common/validation";
+import { AddProfileDto } from "./user.dtos";
 
 const userController = container.resolve<UserController>(UserController)
 export const router = Router();
 
 router.get("/wishlists", protect(true), userController.getUserWishlist);
-router.post("/onboarding", protect(true), userController.onBoardUser)
+router.post("/onboarding", protect(true), RequestValidator.validate(AddProfileDto), userController.onBoardUser)
 router.get("/profile", protect(true), userController.getProfile)
